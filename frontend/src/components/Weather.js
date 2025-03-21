91208e75d468e2f4dc5db25787dbf908701a0101
MaafiaTroodon <malprdt@gmail.com>
2025-03-20 21:13:32 -0300

    Added weather css

--- frontend/src/components/Weather.js
+++ frontend/src/components/Weather.js
@@ -1,6 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./weather.css";
 
 const Weather = () => {
     const [weather, setWeather] = useState(null);
